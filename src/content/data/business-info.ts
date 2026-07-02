6e0d76f72ba986818d169fe36bdeb5e3e0782aa3
chenweijen <wei@chenweijendeMacBook-Pro.local>
2026-07-02 14:13:48 +0800

    feat: add legal business info and LINE contact button

---
+++ src/content/data/business-info.ts
@@ -0,0 +1,13 @@
+export const LINE_ID = "@tour880";
+
+export const LINE_OFFICIAL_URL = "https://lin.ee/VhkewYg";
+
+export const LINE_ADD_FRIEND_IMAGE = "/images/social/line-add-friend.png";
+
+export const LINE_ADD_FRIEND_ALT = "加入樂圖漫遊會館 LINE 好友";
+
+export const LEGAL_BUSINESS_INFO = [
+  "澎湖縣合法民宿編號 NO.392",
+  "統一編號：40809021",
+  "營業人：樂圖漫遊民宿",
+];
